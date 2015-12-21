2e9e3e6b57f6b22b73f0e7fccabb6f8babdc7a1a
Angel Terrones <angelterrones@gmail.com>
2015-12-21 16:00:57 -0430

    Add alu and regfile modules.
    Add a list with the opcodes.

---
+++ Algol/alu.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# Copyright (c) 2015 Angel Terrones (<angelterrones@gmail.com>)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from myhdl import Signal
+from myhdl import concat
+from myhdl import always_comb
+from myhdl import modbv
+
+
+class ALUFunction:
+    OP_ADD  = 0
+    OP_SLL  = 1
+    OP_XOR  = 4
+    OP_SRL  = 5
+    OP_OR   = 6
+    OP_AND  = 7
+    OP_SEQ  = 8
+    OP_SNE  = 9
+    OP_SUB  = 10
+    OP_SRA  = 11
+    OP_SLT  = 12
+    OP_SGE  = 13
+    OP_SLTU = 14
+    OP_SGEU = 15
+
+
+class ALUPortIO:
+    def __init__(self):
+        self.function = Signal(modbv(0)[4:])
+        self.input1 = Signal(modbv(0)[32:])
+        self.input2 = Signal(modbv(0)[32:])
+        self.output = Signal(modbv(0)[32:])
+
+
+class ALU:
+    def __init__(self):
+        self.IO = ALUPortIO()
+
+    def GetRTL(self):
+        io = self.IO
+
+        @always_comb
+        def rtl():
+            shamt = io.input2[5:0]
+            if io.function == ALUFunction.OP_ADD:
+                io.output.next = io.input1 + io.input2
+            elif io.function == ALUFunction.OP_SLL:
+                io.output.next = io.input1 << shamt
+            elif io.function == ALUFunction.OP_XOR:
+                io.output.next = io.input1 ^ io.input2
+            elif io.function == ALUFunction.OP_SRL:
+                io.output.next = io.input1 >> shamt
+            elif io.function == ALUFunction.OP_OR:
+                io.output.next = io.input1 | io.input2
+            elif io.function == ALUFunction.OP_AND:
+                io.output.next = io.input1 & io.input2
+            elif io.function == ALUFunction.OP_SEQ:
+                io.output.next = concat(0, io.input1 == io.input2)
+            elif io.function == ALUFunction.OP_SNE:
+                io.output.next = concat(0, io.input1 != io.input2)
+            elif io.function == ALUFunction.OP_SUB:
+                io.output.next = io.input1 - io.input2
+            elif io.function == ALUFunction.OP_SRA:
+                io.output.next = io.input1.signed() >> shamt
+            elif io.function == ALUFunction.OP_SLT:
+                io.output.next = concat(0, io.input1.signed() < io.input2.signed())
+            elif io.function == ALUFunction.OP_SGE:
+                io.output.next = concat(0, io.input1.signed() >= io.input2.signed())
+            elif io.function == ALUFunction.OP_SLTU:
+                io.output.next = concat(0, io.input1 < io.input2)
+            elif io.function == ALUFunction.OP_SGEU:
+                io.output.next = concat(0, io.input1 >= io.input2)
+            else:
+                io.output.next = 0
+
+        return rtl
+
+    def GetSignals(self):
+        return self.IO
+
+# Local Variables:
+# flycheck-flake8-maximum-line-length: 120
+# flycheck-flake8rc: ".flake8rc"
+# End:
---
+++ Algol/regfile.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# Copyright (c) 2015 Angel Terrones (<angelterrones@gmail.com>)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+from myhdl import Signal
+from myhdl import always
+from myhdl import always_comb
+from myhdl import modbv
+
+
+class RFReadPort:
+    def __init__(self):
+        self.ra = Signal(modbv(0)[5:])
+        self.rd = Signal(modbv(0)[32:])
+
+
+class RFWritePort:
+    def __init__(self):
+        self.wa = Signal(modbv(0)[5:])
+        self.we = Signal(False)
+        self.wd = Signal(modbv(0)[32:])
+
+
+class RegisterFile:
+    def __init__(self):
+        self.clk = Signal(False)
+        self.portA = RFReadPort()
+        self.portB = RFReadPort()
+        self.writePort = RFWritePort()
+        self._registers = [Signal(modbv(0)[32:]) for ii in range(0, 32)]
+
+    def GetRTL(self):
+        clk = self.clk
+        portA = self.portA
+        portB = self.portB
+        writePort = self.writePort
+
+        @always_comb
+        def read():
+            portA.rd.next = self._registers[portA.ra] if portA.ra != 0 else 0
+            portB.rd.next = self._registers[portB.ra] if portB.ra != 0 else 0
+
+        @always(clk.posedge)
+        def write():
+            if writePort.wa != 0 and writePort.we == 1:
+                self._registers[writePort.wa].next = writePort.wd
+
+        return read, write
+
+    def GetSignals(self):
+        return (self.clk, self.portA, self.portB, self.writePort)
+
+# Local Variables:
+# flycheck-flake8-maximum-line-length: 120
+# flycheck-flake8rc: ".flake8rc"
+# End:
