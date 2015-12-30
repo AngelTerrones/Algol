9acc8dc65a2554315cb72a709174637e4c1c2ab8
Angel Terrones <angelterrones@gmail.com>
2015-12-30 17:13:20 -0430

    Add mux2 and mux4.

--- Algol/common/mux.py
+++ Algol/common/mux.py
@@ -43,6 +43,57 @@ class Mux:
 
         return rtl
 
+
+class Mux2:
+    def __init__(self,
+                 sel: Signal,
+                 in1: Signal,
+                 in2: Signal,
+                 out: Signal):
+        self.sel = sel
+        self.in1 = in1
+        self.in2 = in2
+        self.out = out
+
+    def GetRTL(self):
+        @always_comb
+        def rtl():
+            if self.sel == 0:
+                self.out.next = self.in1
+            else:
+                self.out.next = self.in2
+
+        return rtl
+
+
+class Mux4:
+    def __init__(self,
+                 sel: Signal,
+                 in1: Signal,
+                 in2: Signal,
+                 in3: Signal,
+                 in4: Signal,
+                 out: Signal):
+        self.sel = sel
+        self.in1 = in1
+        self.in2 = in2
+        self.in3 = in3
+        self.in4 = in4
+        self.out = out
+
+    def GetRTL(self):
+        @always_comb
+        def rtl():
+            if self.sel == 0:
+                self.out.next = self.in1
+            elif self.sel == 1:
+                self.out.next = self.in2
+            elif self.sel == 2:
+                self.out.next = self.in3
+            else:
+                self.out.next = self.in4
+
+        return rtl
 # Local Variables:
 # flycheck-flake8-maximum-line-length: 120
 # flycheck-flake8rc: ".flake8rc"
