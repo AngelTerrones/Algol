7d6e0dd24a9675794202a922f8f8dd3e0464e7d2
Angel Terrones <angelterrones@gmail.com>
2016-02-02 22:05:01 -0430

    Import from Core, and bug fixes.
    Also, implement the initial loading from an HEX file.

--- Simulation/core/memory.py
+++ Simulation/core/memory.py
@@ -20,13 +20,14 @@
 # THE SOFTWARE.
 from math import ceil
 from math import log
+import os.path
 from myhdl import Signal
 from myhdl import modbv
 from myhdl import always_comb
 from myhdl import always
 from myhdl import instances
-from Algol.memIO import MemoryOpConstant
-from Algol.memIO import MemPortIO
+from Core.memIO import MemoryOpConstant
+from Core.memIO import MemPortIO
 
 
 class Memory:
@@ -36,37 +37,47 @@ class Memory:
                  imem: MemPortIO,
                  dmem: MemPortIO,
                  SIZE: int,
-                 BIN:  str):
+                 HEX:  str):
         assert SIZE >= 2**12, "Memory size must be a positive number. Min value= 4 KB."
-        assert type(BIN) == str and len(BIN) != 0, "Please, indicate a valid name for the bin file."
+        assert type(HEX) == str and len(HEX) != 0, "Please, indicate a valid name for the bin file."
+        assert os.path.isfile(HEX), "HEX file does not exist. Please, indicate a valid name"
 
         aw              = int(ceil(log(SIZE, 2)))
         self.clk        = clk
         self.rst        = rst
         self.imem       = imem
         self.dmem       = dmem
-        self._data_o    = Signal(modbv(0)[32:])
-        self._memory    = [Signal(modbv(0)[32:]) for ii in range(0, 2**aw)]
+        self.i_data_o   = Signal(modbv(0)[32:])
+        self.d_data_o   = Signal(modbv(0)[32:])
+        self._memory    = [None for ii in range(0, 2**(aw - 2))]  # WORDS, no bytes
         self._imem_addr = Signal(modbv(0)[30:])
         self._dmem_addr = Signal(modbv(0)[30:])
 
-        self.LoadMemory(2**aw, BIN)
+        self.LoadMemory(2**(aw - 2), HEX)
 
-    def LoadMemory(size: int, bin_file: str):
-        pass
+    def LoadMemory(self, size: int, bin_file: str):
+        n_lines = int(os.path.getsize(bin_file) / 9)  # Calculate the size in words
+        assert size == n_lines, "Size mismatch: {0} != {1}".format(size, n_lines)
+
+        with open(bin_file) as f:
+            lines = [line.strip() for line in f]
+
+        for addr in range(size):
+            data = int(lines[addr], 16)
+            self._memory[addr] = Signal(modbv(data)[32:])
 
     def GetRTL(self):
-        @always(self.clk)
+        @always(self.clk.posedge)
         def set_ready_signal():
             self.imem.resp.valid.next = False if self.rst else self.imem.req.valid
             self.dmem.resp.valid.next = False if self.rst else self.dmem.req.valid
 
         @always_comb
         def assignment_data_o():
-            self.imem.resp.data.next = self._data_o if self.imem.resp.valid else None
-            self.dmem.resp.data.next = self._data_o if self.dmem.resp.valid else None
+            self.imem.resp.data.next = self.i_data_o if self.imem.resp.valid else 0xDEADF00D
+            self.dmem.resp.data.next = self.d_data_o if self.dmem.resp.valid else 0xDEADF00D
 
-        @always_comb
+        @always(self.clk.posedge)
         def set_fault():
             self.imem.resp.fault.next = False
             self.dmem.resp.fault.next = False
@@ -74,30 +85,30 @@ class Memory:
         @always_comb
         def assignment_addr():
             # This memory is addressed by word, not byte. Ignore the 2 LSB.
-            self._imem_addr = self.imem.req.add[32:2]
-            self._dmem_addr = self.dmem.req.add[32:2]
+            self._imem_addr.next = self.imem.req.addr[32:2]
+            self._dmem_addr.next = self.dmem.req.addr[32:2]
 
-        @always(self.clk)
+        @always(self.clk.posedge)
         def imem_rtl():
-            self._data_o.next = self._memory[self._imem_addr]
+            self.i_data_o.next = self._memory[self._imem_addr]
 
             if self.imem.req.fcn == MemoryOpConstant.M_WR:
                 we                = self.imem.req.wr
                 data              = self.imem.req.data
-                self._data_o.next = self.imem.req.data
+                self.i_data_o.next = self.imem.req.data
                 self._memory[self._imem_addr][8:0].next   = data if we[0] and self.imem.req.valid else self._memory[self._imem_addr][8:0]
                 self._memory[self._imem_addr][16:8].next  = data if we[1] and self.imem.req.valid else self._memory[self._imem_addr][16:8]
                 self._memory[self._imem_addr][24:16].next = data if we[2] and self.imem.req.valid else self._memory[self._imem_addr][24:16]
                 self._memory[self._imem_addr][32:24].next = data if we[3] and self.imem.req.valid else self._memory[self._imem_addr][32:24]
 
-        @always(self.clk)
+        @always(self.clk.posedge)
         def dmem_rtl():
-            self._data_o.next = self._memory[self._imem_addr]
+            self.d_data_o.next = self._memory[self._dmem_addr]
 
             if self.dmem.req.fcn == MemoryOpConstant.M_WR:
                 we                = self.dmem.req.wr
                 data              = self.dmem.req.data
-                self._data_o.next = self.dmem.req.data
+                self.d_data_o.next = self.dmem.req.data
                 self._memory[self._dmem_addr][8:0].next   = data if we[0] and self.dmem.req.valid else self._memory[self._dmem_addr][8:0]
                 self._memory[self._dmem_addr][16:8].next  = data if we[1] and self.dmem.req.valid else self._memory[self._dmem_addr][16:8]
                 self._memory[self._dmem_addr][24:16].next = data if we[2] and self.dmem.req.valid else self._memory[self._dmem_addr][24:16]
