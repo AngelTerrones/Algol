8b73b495a217e3d63b741a0a4519f09626d721aa
Angel Terrones <angelterrones@gmail.com>
2016-02-29 17:03:30 -0430

    WIP: refactor this.

--- Core/icache.py
+++ Core/icache.py
@@ -63,95 +63,72 @@ def ICache(clk,
 
     # --------------------------------------------------------------------------
     # params
-    WAY_WIDTH = BLOCK_WIDTH + SET_WIDTH
-    TAG_WIDTH = LIMIT_WIDTH - WAY_WIDTH
+    WAY_WIDTH = BLOCK_WIDTH + SET_WIDTH  # cache mem address width
+    TAG_WIDTH = LIMIT_WIDTH - WAY_WIDTH  # tag size
     # width and index for tags
-    TAGMEM_WAY_WIDTH = TAG_WIDTH + 1
-    TAGMEM_WAY_VALID = TAGMEM_WAY_WIDTH - 1
+    TAGMEM_WAY_WIDTH = TAG_WIDTH + 1         # Add the valid bit
+    TAGMEM_WAY_VALID = TAGMEM_WAY_WIDTH - 1  # Valid bit index
     # calculate the needed LRU bits (from mor1kx_icache.v)
     TAG_LRU_WIDTH = (WAYS * (WAYS - 1)) >> 1  # (N*(N-1))/2
-    TAG_LRU_WIDTH_BITS = TAG_LRU_WIDTH if WAYS >= 2 else 1
+    # TAG_LRU_WIDTH_BITS = TAG_LRU_WIDTH if WAYS >= 2 else 1
     # Size of tag memory
-    TAGMEM_WIDTH = (TAGMEM_WAY_WIDTH * WAYS) + TAG_LRU_WIDTH
+    TAGMEM_WIDTH = (TAGMEM_WAY_WIDTH * WAYS) + TAG_LRU_WIDTH  # width of one tag line.
     # --------------------------------------------------------------------------
 
     ic_states = enum('CHECK', 'FETCH', 'WAIT', 'FLUSH', 'FLUSH_LAST', encoding='one_hot')
 
-    tag_read_port     = RAMIOPort(A_WIDTH=SET_WIDTH, D_WIDTH=TAGMEM_WIDTH)
-    tag_update_port   = RAMIOPort(A_WIDTH=SET_WIDTH, D_WIDTH=TAGMEM_WIDTH)
+    tag_rw_port       = RAMIOPort(A_WIDTH=SET_WIDTH, D_WIDTH=TAGMEM_WIDTH)
+    tag_flush_port    = RAMIOPort(A_WIDTH=SET_WIDTH, D_WIDTH=TAGMEM_WIDTH)
     cache_read_port   = [RAMIOPort(A_WIDTH=WAY_WIDTH, D_WIDTH=D_WIDTH) for _ in range(0, WAYS)]
     cache_update_port = [RAMIOPort(A_WIDTH=WAY_WIDTH, D_WIDTH=D_WIDTH) for _ in range(0, WAYS)]
+
     state             = Signal(ic_states.CHECK)
     n_state           = Signal(ic_states.CHECK)
 
     busy              = Signal(False)
 
-    tag_address       = Signal(modbv(0)[TAG_WIDTH:])
-    tag_in            = [Signal(0)[TAG_WIDTH:] for _ in range(0, WAYS)]
-    tag_out           = [Signal(0)[TAG_WIDTH:] for _ in range(0, WAYS)]
-
-    cache_mem_addr    = Signal(modbv(0)[WAY_WIDTH:])
-
-    miss_w            = [Signal(False) for _ in range(0, WAYS)]
-    miss_w_and        = Signal(False)
     miss              = Signal(False)
-
+    miss_w            = Signal(modbv(0)[WAYS:])
+    miss_w_and        = Signal(False)
     flush             = Signal(False)
-    n_flush           = Signal(False)
-    flush_addr        = Signal(modbv(0)[LIMIT_WIDTH:])
-    flush_we          = Signal(False)
+    final_fetch       = Signal(False)
     final_flush       = Signal(False)
 
-    final_fetch       = Signal(False)
-    fetch_addr        = Signal(modbv(0)[LIMIT_WIDTH:])
+    lru_select        = Signal(modbv(0)[WAYS:])
 
-    @always_comb
-    def assigments():
-        busy.next           = state != ic_states.CHECK
-        final_flush.next    = tag_update_port.addr == 0
-        cache_mem_addr.next = cpu.addr[WAY_WIDTH:2]
+    # tag in/out signals: For data assignment
+    tag_in            = [Signal(modbv(0))[TAGMEM_WAY_WIDTH:] for _ in range(0, WAYS)]
+    tag_out           = [Signal(modbv(0))[TAGMEM_WAY_WIDTH:] for _ in range(0, WAYS)]
+    lru_in            = Signal(modbv(0))[TAG_LRU_WIDTH:]
+    lru_out           = Signal(modbv(0))[TAG_LRU_WIDTH:]
 
-    @always_comb
-    def cpu_assignments():
-        # cpu data_in assignment: instruction.
-        cpu.rdata.next = 0x0BADF00D
-        for i in range(0, WAYS):
-            if not miss_w[i]:
-                cpu.rdata.next = cache_read_port[i].data_o
-
-        # cpu ready signal assigment: the module is not busy (flush/refill) and finished
-        # memory transactions.
-        cpu.ready.next = (False if busy else
-                          (not miss_w_and if cpu.fnc == MemOp.M_RD and cpu.valid else
-                           False))
-        # cpu fault assignment
-        cpu.fault.next = mem.fault
-
-    @always_comb
-    def mem_assignments():
-        pass
-
-    @always_comb
-    def tag_assigments():
-        for i in range(0, WAYS):
-            tag_out[i].next = tag_read_port.data_o[TAGMEM_WAY_WIDTH * (i + 1):TAGMEM_WAY_WIDTH * i]
-            tag_read_port.data_i[TAGMEM_WAY_WIDTH * (i + 1):TAGMEM_WAY_WIDTH * i].next = tag_in[i]
-        tag_address.next      = cpu.addr[SET_WIDTH:0]
-        tag_read_port.we.next = False
-
-    @always_comb
-    def cache_mem_assignment():
-        for i in range(0, WAYS):
-            cache_read_port[i].addr.next   = cache_mem_addr
-            cache_read_port[i].data_i.next = 0xDEADC0DE
-            cache_read_port[i].we.next     = False
+    # refill signals
+    refill_addr       = Signal(0)[LIMIT_WIDTH:]
+    refill_valid      = Signal(False)
 
     @always_comb
     def miss_check():
+        """
+        For each way, check tag and valid flag, and reduce the vector using AND.
+        If the vector is full of ones, the data is not in the cache: assert the miss flag.
+
+        MISS: data not in cache and the memory operation is a valid read. Ignore this if
+        the module is flushing data.
+        """
         for i in range(0, WAYS):
-            miss_w[i].next = (not tag_out[i][TAGMEM_WAY_VALID] or tag_out[i] != cpu.addr[LIMIT_WIDTH:WAY_WIDTH])
+            miss_w[i].next = (not tag_out[i][TAGMEM_WAY_VALID] or tag_out[i][TAGMEM_WAY_WIDTH:0] != cpu.addr[LIMIT_WIDTH:WAY_WIDTH])
         miss_w_and.next = reduce(lambda x, y: x and y, miss_w)
-        miss.next = miss_w_and and cpu.fcn == MemOp.M_RD and not flush and not invalidate
+        miss.next = miss_w_and and (cpu.fcn == MemOp.M_RD) and not flush and not invalidate
+
+    @always_comb
+    def tag_rport():
+        # for each way, assign tags
+        for i in range(0, WAYS):
+            tag_out[i].next = tag_rw_port.data_o[TAGMEM_WAY_WIDTH * (i + 1):TAGMEM_WAY_WIDTH * i]
+            tag_rw_port.data_i[TAGMEM_WAY_WIDTH * (i + 1):TAGMEM_WAY_WIDTH * i].next = tag_in[i]
+        # connect to the lru field for reading and updating
+        lru_out.next = tag_rw_port.data_o[TAGMEM_WIDTH:(TAGMEM_WAY_WIDTH * WAYS)]
+        tag_rw_port.data_i[TAGMEM_WIDTH:(TAGMEM_WAY_WIDTH * WAYS)].next = lru_in
 
     @always_comb
     def next_state_logic():
@@ -167,14 +144,17 @@ def ICache(clk,
                 # Hit or read request
                 n_state.next = ic_states.CHECK
         elif state == ic_states.FETCH:
+            # fetch a line from memory
             if final_fetch:
                 n_state.next = ic_states.WAIT
         elif state == ic_states.FLUSH:
+            # invalidate tag memory
             if final_flush:
                 n_state.next = ic_states.FLUSH_LAST
             else:
                 n_state.next = ic_states.FLUSH
         elif state == ic_states.FLUSH_LAST:
+            # last cycle for flush
             n_state.next = ic_states.WAIT
         elif state == ic_states.WAIT:
             n_state.next = ic_states.CHECK
@@ -186,53 +166,35 @@ def ICache(clk,
         else:
             state.next = n_state
 
-    @always(clk.posedge)
-    def refill_fsm():
-        pass
-
     @always_comb
-    def flush_next_state():
-        flush_addr.next = tag_update_port.addr
-        flush_we.next   = tag_update_port.we
-        n_flush.next    = flush
-
-        if state == ic_states.CHECK:
-            if flush or invalidate:
-                flush_addr.next = modbv(0xFFFFFFFF)[SET_WIDTH:]
-                flush_we.next   = True
-                n_flush.next    = False
-        elif state == ic_states.FLUSH:
-            flush_addr.next = tag_update_port.addr - 1
-            flush_we.next   = True
-        elif state == ic_states.FLUSH_LAST:
-            flush_we.next = False
-        else:
-            # latch the invalidate signal
-            n_flush.next = invalidate
-
-    @always(clk.posedge)
-    def flush_update():
-        if rst:
-            tag_update_port.addr.next = modbv(0xFFFFFFFF)[SET_WIDTH:]
-            tag_update_port.we.next   = False
-            flush.next                = False
-        else:
-            tag_update_port.addr.next = flush_addr
-            tag_update_port.we.next   = flush_we
-            flush.next                = n_flush
-
-    @always_comb
-    def tag_write():
+    def cpu_port_assign():
+        """
+        Assignments to the cpu interface.
+        """
+        # cpu data_in assignment: instruction.
+        cpu.rdata.next = 0x0BADF00D
         for i in range(0, WAYS):
-            tag_in[i].next          = tag_out[i]
-        tag_update_port.we.next = False
+            if not miss_w[i]:
+                cpu.rdata.next = cache_read_port[i].data_o
 
-        if state == ic_states.CHECK:
-            if miss:
-                pass
-            else:
-                # update LRU
-                pass
+        # cpu ready signal assigment: the module is not busy (flush/refill) and finished
+        # memory transactions.
+        cpu.ready.next = (False if busy else
+                          (not miss_w_and if cpu.fnc == MemOp.M_RD and cpu.valid else
+                           False))
+        # cpu fault assignment
+        cpu.fault.next = mem.fault
+
+    @always_comb
+    def mem_port_assign():
+        """
+        Assignments to the mem interface
+        """
+        mem.addr.next  = refill_addr
+        mem.wdata.next = 0x0BADF00D
+        mem.wr.next    = modbv(0)[4:]
+        mem.fcn.next   = MemOp.M_RD
+        mem.valid.next = refill_valid
 
     @always_comb
     def no_cache():
@@ -246,8 +208,8 @@ def ICache(clk,
         cpu.fault.next = mem.fault
 
     # Instantiate memory
-    tag_mem = RAM_DP(tag_read_port,
-                     tag_update_port,
+    tag_mem = RAM_DP(tag_rw_port,
+                     tag_flush_port,
                      A_WIDTH=SET_WIDTH,
                      D_WIDTH=TAGMEM_WIDTH)
 
@@ -260,7 +222,7 @@ def ICache(clk,
 
     # Bypass the cache if this module is disabled.
     if ENABLE:
-        return next_state_logic, update_state, tag_mem, cache_mem
+        return tag_mem, cache_mem
     else:
         return no_cache
 
