c91c9804add5112d92c489ce7d4691f29cc04961
Angel Terrones <angelterrones@gmail.com>
2016-03-12 13:53:22 -0430

    Set 2 top modules: Core y CoreHDL.
    Core: Top module for simulation and SoC implementations using only
    MyHDL.
    CoreHDL: Topr module for conversion to Verilog (avoiding the ugly naming
    of port signals)

--- Core/core.py
+++ Core/core.py
@@ -19,9 +19,12 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+from myhdl import always_comb
 from Core.dpath import Datapath
 from Core.cpath import Ctrlpath
 from Core.cpath import CtrlIO
+from Core.wishbone import WishboneMaster
+from Core.wishbone import WishboneIntercon
 
 
 def Core(clk,
@@ -31,6 +34,7 @@ def Core(clk,
          toHost):
     """
     Core top module.
+    This module use interfaces, for use in an integrated SoC.
 
     :param clk:    System clock
     :param rst:    System reset
@@ -51,6 +55,76 @@ def Core(clk,
                      dmem)
 
     return dpath, cpath
+
+
+def CoreHDL(clk,
+            rst,
+            toHost,
+            imem_addr_o,
+            imem_dat_o,
+            imem_sel_o,
+            imem_cti_o,
+            imem_cyc_o,
+            imem_we_o,
+            imem_stb_o,
+            imem_dat_i,
+            imem_stall_i,
+            imem_ack_i,
+            imem_err_i,
+            dmem_addr_o,
+            dmem_dat_o,
+            dmem_sel_o,
+            dmem_cti_o,
+            dmem_cyc_o,
+            dmem_we_o,
+            dmem_stb_o,
+            dmem_dat_i,
+            dmem_stall_i,
+            dmem_ack_i,
+            dmem_err_i):
+    """
+    Core top Module.
+    This module use single ports, for verilog translation, and avoid
+    generating ugly names for top ports.
+    """
+
+    imem = WishboneIntercon()
+    dmem = WishboneIntercon()
+    imem_m = WishboneMaster(imem)
+    dmem_m = WishboneMaster(dmem)
+    core = Core(clk=clk,
+                rst=rst,
+                toHost=toHost,
+                imem=imem_m,
+                dmem=dmem_m)
+
+    @always_comb
+    def assign():
+        imem_addr_o.next    = imem_m.addr_o
+        imem_dat_o.next     = imem_m.dat_o
+        imem_sel_o.next     = imem_m.sel_o
+        imem_cti_o.next     = imem_m.cti_o
+        imem_cyc_o.next     = imem_m.cyc_o
+        imem_we_o.next      = imem_m.we_o
+        imem_stb_o.next     = imem_m.stb_o
+        imem_m.dat_i.next   = imem_dat_i
+        imem_m.stall_i.next = imem_stall_i
+        imem_m.ack_i.next   = imem_ack_i
+        imem_m.err_i.next   = imem_err_i
+
+        dmem_addr_o.next    = dmem_m.addr_o
+        dmem_dat_o.next     = dmem_m.dat_o
+        dmem_sel_o.next     = dmem_m.sel_o
+        dmem_cti_o.next     = dmem_m.cti_o
+        dmem_cyc_o.next     = dmem_m.cyc_o
+        dmem_we_o.next      = dmem_m.we_o
+        dmem_stb_o.next     = dmem_m.stb_o
+        dmem_m.dat_i.next   = dmem_dat_i
+        dmem_m.stall_i.next = dmem_stall_i
+        dmem_m.ack_i.next   = dmem_ack_i
+        dmem_m.err_i.next   = dmem_err_i
+
+    return core, assign
 
 # Local Variables:
 # flycheck-flake8-maximum-line-length: 120
